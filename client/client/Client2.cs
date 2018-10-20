4ed758a14835657ac835b4c8b14d8ce779eca732
papasani-soft <papasani.soft@gmail.com>
2018-10-20 23:58:06 +0530

    commit

---
+++ client/client/Client2.cs
@@ -0,0 +1,49 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Text;
+
+namespace MSClient
+{
+    class Client2
+    {
+        const int PORT_NO = 2201;
+        const string SERVER_IP = "127.0.0.1";
+        static void Main(string[] args)
+        {
+            //---data to send to the server---
+            Console.WriteLine("Enter firstname");
+            string firstName = Console.ReadLine();
+            Console.WriteLine("Enter lastname");
+            string lastName = Console.ReadLine();
+            Console.WriteLine("Enter age");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Details d = new Details();
+            d.firstName = firstName;
+            d.lastName = lastName;
+            d.age = age;
+            string s = JsonConvert.SerializeObject(d);
+            Console.WriteLine(s);
+            //---create a TCPClient object at the IP and port no.---
+            TcpClient client = new TcpClient(SERVER_IP, PORT_NO);
+            NetworkStream nwStream = client.GetStream();
+            byte[] bytesToSend = ASCIIEncoding.ASCII.GetBytes(s);
+            //---send the text---
+            Console.WriteLine("Sending : " + s);
+            nwStream.Write(bytesToSend, 0, bytesToSend.Length);
+            //---read back the text---
+            byte[] bytesToRead = new byte[client.ReceiveBufferSize];
+            //int bytesRead = nwStream.Read(bytesToRead, 0, client.ReceiveBufferSize);
+           // Console.WriteLine("Received : " + Encoding.ASCII.GetString(bytesToRead, 0, bytesRead));
+            Console.ReadLine();
+            client.Close();
+        }
+    }
+    class Details
+    {
+       public string firstName;
+       public string lastName;
+        public int age;
+    }
+}
---
+++ sercli/sercli/Server2.cs
@@ -0,0 +1,53 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace MulConServ
+{
+    class Server2
+    {
+        const int PORT_NO = 2201;
+        const string SERVER_IP = "127.0.0.1";
+        static void Main(string[] args)
+        {
+            //---listen at the specified IP and port no.---
+            IPAddress localAdd = IPAddress.Parse(SERVER_IP);
+            TcpListener listener = new TcpListener(localAdd, PORT_NO);
+            Console.WriteLine("Listening...");
+            listener.Start();
+            //---incoming client connected---
+            TcpClient client = listener.AcceptTcpClient();
+            //---get the incoming data through a network stream---
+            NetworkStream nwStream = client.GetStream();
+            byte[] buffer = new byte[client.ReceiveBufferSize];
+            //---read incoming stream---
+            int bytesRead = nwStream.Read(buffer, 0, client.ReceiveBufferSize);
+            //---convert the data received into a string---
+            string dataReceived = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+            Console.WriteLine("Received : " + dataReceived);
+
+            Details d=JsonConvert.DeserializeObject<Details>(dataReceived);
+            Console.WriteLine("desrialisation started");
+            Console.WriteLine("first name:"+d.firstName);
+            Console.WriteLine("last name:"+d.lastName);
+            Console.WriteLine("age:"+d.age);
+            Console.WriteLine("end");
+
+            //---write back the text to the client---
+           
+            nwStream.Write(buffer, 0, bytesRead);
+            client.Close();
+            listener.Stop();
+            Console.ReadLine();
+        }
+    }
+    class Details
+    {
+        public string firstName;
+        public string lastName;
+        public int age;
+    }
+}
